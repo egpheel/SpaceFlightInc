4f89a1a5b964f6ef431a98f3a4bffbb089857d5a
Eduardo Gil <egpheel@pop-os.localdomain>
2019-09-20 19:00:06 +0100

    initial travel and jump distance code

--- js/components.js
+++ js/components.js
@@ -4,7 +4,7 @@ var player = {
     location: 3,
     status: 0,
     travellingTo: 3,
-    travelTime: 0
+    arrivalTime: 0
 }
 
 var statuses = ["Docked", "Travelling"]
@@ -23,8 +23,9 @@ var planets = [
 ]
 
 var spaceships = [
-    { name: "Cessna Spc-Rocket", timeMultiplier: 1, capacity: 5, price: 250 },
-    { name: "Airbus A9000", timeMultiplier: 0.75, capacity: 100, price: 3500 }
+    { name: "Cessna Spc-Rocket", timeMultiplier: 1, jumpDistance: 10, capacity: 5, price: 250 },
+    { name: "Airbus A9000", timeMultiplier: 0.75, jumpDistance: 25, capacity: 100, price: 3500 },
+    { name: "Boeing 9000", timeMultiplier: 0.80, jumpDistance: 37, capacity: 130, price: 3750 }
 ]
 
 export { player, statuses, planets, spaceships }
--- js/update_functions.js
+++ js/update_functions.js
@@ -20,6 +20,10 @@ function updateStats() {
         else if (stat.innerHTML.startsWith('Speed', 6)) {
             stat.innerHTML = '<span>Speed: <span>' + spaceships[player.spaceship].timeMultiplier + '</span> sec/dunit</span>'
         }
+        // update spaceship's jump distance
+        else if (stat.innerHTML.startsWith('Jump Distance', 6)) {
+            stat.innerHTML = '<span>Jump Distance: <span>' + spaceships[player.spaceship].jumpDistance + '</span> dunits</span>'
+        }
         // update spaceship's passenger capacity
         else if (stat.innerHTML.startsWith('Passenger', 6)) {
             stat.innerHTML = '<span>Passenger Capacity: <span>' + spaceships[player.spaceship].capacity + '</span></span>'
@@ -35,22 +39,103 @@ function updateStats() {
     }
 }
 
-function createPlanetDiv(name, distance) {
+function createPlanetDiv(index, distance) {
     let container = document.getElementsByClassName('trav-c')
+    
+    let div = document.createElement('div')
+    div.setAttribute('class', 'dest-c')
+    div.innerHTML = '<span>' + planets[index].name + '</span><span>Distance: ' + distance + ' dunits</span>'
 
-    container[0].innerHTML += '<div class="dest-c"><span>' + name + '</span><span>Distance: ' + distance + ' dunits</span><button>Travel</button></div>'
+    let btn = document.createElement('button')
+    let btnText = document.createTextNode('Travel')
+    btn.setAttribute('class', 'travelBtn')
+    
+    if (distance > spaceships[player.spaceship].jumpDistance) {
+        btn.setAttribute('disabled', 'true')
+        btnText = document.createTextNode('Too far')
+    }
+
+    btn.appendChild(btnText)
+
+    btn.addEventListener('click', function() {
+        travelTo(index, distance)
+    })
+
+    div.appendChild(btn)
+
+    container[0].appendChild(div)
+}
+
+function travelTo(index, distance) {
+    player.location = 0
+    player.status = 1
+    player.travellingTo = index
+
+    updateStats()
+
+    var travelTime = Math.round(distance * spaceships[player.spaceship].timeMultiplier)
+
+    let date = new Date()
+    let now = date.getTime()
+
+    player.arrivalTime = now + travelTime * 1000
+
+    clearDestinations()
+
+    startTravel(index)
+}
+
+function startTravel(index) {
+    var date = new Date()
+    var now = date.getTime()
+    var travelTime = Math.round((player.arrivalTime - now)/1000)
+
+    var container = document.getElementsByClassName('trav-c')
+
+    container[0].innerHTML = travelTime + " seconds left to arrive at " + planets[index].name
+
+    var timeLeft = setInterval(function() {
+        date = new Date()
+        now = date.getTime()
+        travelTime = Math.round((player.arrivalTime - now)/1000)
+
+        container[0].innerHTML = travelTime + " seconds left to arrive at " + planets[index].name
+
+        if (travelTime <= 0) {
+            clearInterval(timeLeft)
+            
+            player.location = index
+            player.status = 0
+            
+            container[0].innerHTML = ''
+
+            updateStats()
+            updateDestinations()
+        }
+    }, 1000)
+}
+
+function clearDestinations() {
+    let container = document.getElementsByClassName('trav-c')
+    
+    container[0].innerHTML = ''
 }
 
 function updateDestinations() {
-    planets.forEach(function(item, index) {
-        if (planets[index].distance !== -1 && index !== player.location) {
-            var distance = Math.abs(
-                planets[player.location].distance - planets[index].distance
-            )
-
-            createPlanetDiv(planets[index].name, distance)
-        }
-    })
+    // if travelling, do not update destinations and start travel countdown
+    if (player.status === 1) {
+        startTravel(player.travellingTo)
+    } else {
+        planets.forEach(function(item, index) {
+            if (planets[index].distance !== -1 && index !== player.location) {
+                var distance = Math.abs(
+                    planets[player.location].distance - planets[index].distance
+                )
+    
+                createPlanetDiv(index, distance)
+            }
+        })
+    }
 }
 
 export { updateStats, updateDestinations }
